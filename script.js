f02a8bf9913400f24d97d1e8739f3f12950caf5a
reyvandresta <reyvandresta77@gmail.com>
2024-09-03 20:34:32 +0700

    add fitur login

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('loginform').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const correctUsername = 'Reyvan';
+    const correctPassword = 'Reyvan';
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    
+    if (username === correctUsername && password === correctPassword) {
+        alert('Login Berhasil');
+        console.log('Berhasil');
+        window.location.href = 'index.html';
+    } else {
+        alert('Login Gagal');
+        console.log('Gagal');
+    }
+});
